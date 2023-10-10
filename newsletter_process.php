5fd4d335a1a32eb12c8d033df90589ca273adb1a
Hristina <hristinastankova89@gmail.com>
2023-10-10 07:07:50 +0200

    update comments

--- newsletter_process.php
+++ newsletter_process.php
@@ -4,24 +4,18 @@ require_once 'functions.php';
 require_once 'functions/newsletter.php';
 
 // Return early pattern
-// Situation désirée : la clé email est définie dans le tableau $_POST
-// J'inverse donc cette condition
-// et suis capable d'interrompre le script (ou rediriger, comme ici)
-// beaucoup plus tôt
+
 if (!isset($_POST['email'])) {
     redirect('newsletter.php');
 }
 
-// Si je me trouve ici, ça signifie que je n'ai pas redirigé
-// Donc que j'ai bien une clé email dans $_POST
-// 1 - Initialisations de variables
 $email = $_POST['email'];
 $emailsFilePath = __DIR__ . '/emails.txt';
 $spamDomainsFilePath = __DIR__ . '/spam_domains.txt';
 
 // 2 - Validations
 
-// S'assurer que l'email n'est pas vide
+// S'assurer que l'email n'est pas vide: 
 if (empty($email)) {
     redirect("newsletter.php?error=" . EMAIL_EMPTY);
 }
